560ac0d92434ab2f30784f2ad7f50cdbd46ebff9
Moshe Gelberg <glbrgm@gmail.com>
2020-07-08 07:59:42 +0300

    10 - pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,43 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Solution 1 (iterate)
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' '
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+// Solution 2 (recursion)
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  let add;
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
