ddd5d5812a8ca3874859b619909a03e984b12067
Moshe Gelberg <glbrgm@gmail.com>
2020-07-10 12:38:27 +0300

    17 - qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,41 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  add(record) {
+    this.stack1.push(record);
+  }
+
+  remove() {
+    this.swap(this.stack1, this.stack2);
+
+    const result = this.stack2.pop();
+
+    this.swap(this.stack2, this.stack1);
+
+    return result;
+  }
+
+  peek() {
+    this.swap(this.stack1, this.stack2);
+
+    const result = this.stack2.peek();
+
+    this.swap(this.stack2, this.stack1);
+
+    return result;
+  }
+
+  swap(stackA, stackB) {
+    while (stackA.peek()) {
+      stackB.push(stackA.pop());
+    }
+  }
+}
 
 module.exports = Queue;
